46c0e22778965100f86ac5acaf46519ec1f0219d
neonelemental <mike@oodle.io>
2018-12-15 16:05:46 -0500

    Adds logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <main id="app">
     <sidebar>
+      <img class='logo' src="./assets/images/image.jpeg"/>
       <a href="javascript:void(0)">hello</a>
     </sidebar>
     <router-view/>
