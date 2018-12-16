b42d74f4273e4d3051fab6f866f9c885ec01f27f
neonelemental <mike@oodle.io>
2018-12-16 01:10:21 -0500

    Starting to think about themeing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <main id="app">
-    <sidebar>
-      <img class='logo' src="./assets/images/image.jpeg"/>
-      <a href="javascript:void(0)">hello</a>
-    </sidebar>
+  <main id="app" class='animated' :class="theme.dark ? 'theme-dark' : 'theme-light'">
+    <a id="theme-picker" class="btn small" href="javascript:void(0)" @click="toggleTheme">
+      <font-awesome-icon :icon="themeIcon"/>
+    </a>
     <router-view/>
   </main>
 </template>
@@ -14,6 +13,24 @@
   export default {
     components: {
       Sidebar
+    },
+    data () {
+      return {
+        theme: {
+          dark: true,
+          light: false
+        }
+      }
+    },
+    computed: {
+      themeIcon () {
+        return this.theme.dark ? 'sun' : 'moon'
+      }
+    },
+    methods: {
+      toggleTheme() {
+        [this.theme.dark, this.theme.light] = this.theme.dark ? [false, true] : [true, false]
+      }
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 import store from './store'
 import './assets/stylesheets/app.scss'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faMoon, faSun)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 Vue.config.productionTip = false
 
 new Vue({
